3c7852f0d2b3b9fc6b4c5dd468bda10c1da521c1
mrKornelius <magnus.korneliusson@gmail.com>
2021-10-13 00:08:38 +0200

    Most functionality is done.

--- script.js
+++ script.js
@@ -1,68 +1,83 @@
+let y = '';
+let op = null;
+let lastPressedBtn = null;
+/*  Extra functions
+ *      1/x, (-), '.', memo, clear, 
+ */
 
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => {
+    if (b === 0) return NaN;
+    return a / b
+};
+const pow = a => a * a;
+const sqrt = a => Math.sqrt(a);
+const sign = a => -a;
+const percent = a => a / 100;
 
-    const operate = (op, a, b) => op(a,b);
+const operate = (op, a, b) => op(a,b);
 
 
-    const x = document.querySelector('input');
-    const eq = document.querySelector('.eq');
-    const pl = document.querySelector('.add');
-    const mi = document.querySelector('.sub');
-    const mu = document.querySelector('.mul');
-    const di = document.querySelector('.div');
-    
-    let y = '';
-    let op;
+const x = document.querySelector('input');
+const eq = document.querySelector('.eq');
+const pl = document.querySelector('.add');
+const mi = document.querySelector('.sub');
+const mu = document.querySelector('.mul');
+const di = document.querySelector('.div');
 
-    pl.addEventListener('click', e => {
-        if (y === '') {
-            y = +x.value;
-            x.value = '';
-            op = add;
-        } else {
-            x.value = operate(op, y, +x.value);
-            y = +x.value;
-            op = add;
-        }
-    });
-    mi.addEventListener('click', e => {
-        if (y === '') {
-            y = +x.value;
-            x.value = '';
-            op = sub;
-        } else {
-            x.value = operate(op, y, +x.value);
-            y = +x.value;
-            op = sub;
-        }
-    });
-    mu.addEventListener('click', e => {
-        if (y === '') {
-            y = +x.value;
-            x.value = '';
-            op = mul;
-        } else {
-            x.value = operate(op, y, +x.value);
-            y = +x.value;
-            op = mul;
-        }
-    });
-    di.addEventListener('click', e => {
-        if (y === '') {
-            y = +x.value;
-            x.value = '';
-            op = div;
-        } else {
-            x.value = operate(op, y, +x.value);
-            y = +x.value;
-            op = div;
-        }
-    });
+x.value = '0';
 
-    eq.addEventListener('click', e => {
-        x.value = operate(op,y,+x.value);
-        y = '';
-    });
+const nums = Array.from(document.querySelectorAll('.num'));
+nums.forEach(btn => btn.addEventListener('click', () => {
+    if (x.value === '0' || lastPressedBtn.includes('operator') || op === null) {
+        x.value = '';
+    }
+    x.value += btn.textContent;
+    lastPressedBtn = btn.classList.value;
+}));
+
+const operators = Array.from(document.querySelectorAll('.operator'));
+operators.forEach(btn => btn.addEventListener('click', () => {
+    // console.log(btn.textContent);
+
+    if (y === '') {
+        y = +x.value;
+    } else {
+        x.value = operate(op, y, +x.value);
+        y = +x.value;
+    }
+
+    switch (btn.textContent) {
+        case '+':    op = add; break;
+        case '-':    op = sub; break;
+        case 'x':    op = mul; break;
+        case '/':    op = div; break;
+        case '^2':   op = pow; 
+                     equals(); 
+                     break;
+        case 'sqrt': op = sqrt;
+                     equals();    
+                     break;
+        case '±':    op = sign;
+                     equals();    
+                     break;
+        case '%':    op = percent;
+                     equals();    
+                     break;
+        default: 
+            console.log('Error button: ' + btn.textContent);
+    }
+        
+    lastPressedBtn = btn.classList.value;
+}));
+
+function equals() {
+    if (op === null) return;
+    x.value = operate(op,y,+x.value);
+    op = null;
+    y = '';
+}
+eq.addEventListener('click', equals);
+
