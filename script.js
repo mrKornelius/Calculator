a1f3fd2f17acaf1ff1b242f43e2a545dfa96c1ff
mrKornelius <magnus.korneliusson@gmail.com>
2021-10-13 23:06:18 +0200

    Added the possibility to change operator during a calc and op-btn highlighting.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let y = '';
 let op = null;
 let lastPressedBtn = null;
-/*  Extra functions
- *      1/x, '.', backspace, PI, x^y, x!, floor/ceil?
+
+/*  TODO: Extra functions
+ *      1/x, '.'(floating point), backspace, PI, x^y, x!( max 18!), floor/ceil?, overflow
+ *      light theme, keyboard support
  */
 
 const add = (a, b) => a + b;
@@ -12,9 +14,11 @@ const div = (a, b) => {
     if (b === 0) return NaN;
     return a / b
 };
+const inv = a => div(1, a); 
 const pow = a => a * a;
 const sqrt = a => Math.sqrt(a);
 const sign = a => -a;
+const factorial = a => fact(a);
 const percent = a => a / 100;
 const operate = (op, a, b) => op(a,b);
 
@@ -22,7 +26,6 @@ const x = document.querySelector('input');
 const eq = document.querySelector('.eq');
 const ac = document.querySelector('.allclear');
 const neg = document.querySelector('.neg');
-
 
 const nums = Array.from(document.querySelectorAll('.num'));
 const ops = Array.from(document.querySelectorAll('.operator'));
@@ -35,6 +38,12 @@ function equals() {
     x.value = operate(op,y,+x.value);
     op = null;
     y = '';
+    clearOperatorHighlight()
+}
+
+function fact(n) {
+    if (n <= 0) return 1;
+    return n * fact(n-1);
 }
 
 function allClear() {
@@ -42,8 +51,14 @@ function allClear() {
     op = null;
     y = '';
     lastPressedBtn = null;
+    clearOperatorHighlight()
 }
 
+function clearOperatorHighlight() {
+    ops.forEach(btn => btn.classList.remove('highlight'));
+}
+
+// TODO: bug ex '3' '+' '3' '=' then press a number ex '2' --> '62' but should be just '2'
 function numbers() {
     if (x.value === '0' || lastPressedBtn.includes('operator') ){//|| op === null) {
         x.value = '';
@@ -59,15 +74,18 @@ function numbers() {
 }
 
 function operators() {
-    // console.log(btn.textContent);
+    clearOperatorHighlight();
 
-    if (y === '') {
-        y = +x.value;
-    } else {
-        x.value = operate(op, y, +x.value);
-        y = +x.value;
+    if (!lastPressedBtn.includes('operator')) {
+        if (y === '') {
+            y = +x.value;
+        } else {
+            x.value = operate(op, y, +x.value);
+            y = +x.value;
+        }
     }
 
+    // switch on id instead??
     switch (this.textContent) {
         case '+':    op = add; break;
         case '-':    op = sub; break;
@@ -90,6 +108,8 @@ function operators() {
     }
 
     lastPressedBtn = this.classList.value;
+    if (!this.classList.value.includes('unary'))
+        this.classList.add('highlight');
 }
 
 // Event Listeners
@@ -97,5 +117,4 @@ nums.forEach(btn => btn.addEventListener('click', numbers));
 ops.forEach(btn => btn.addEventListener('click', operators));
 eq.addEventListener('click', equals);
 ac.addEventListener('click', allClear);
-
 neg.addEventListener('click', numbers);
